c642a290eb7d0b166d72356582a85eae6aecad3e
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2026-05-22 03:34:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Rotate Array by One/rotate-array-by-one.cpp
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.cpp
@@ -1,11 +1,7 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
 
 
-// } Driver Code Ends
+
+
 // User function Template for C++
 
 class Solution {
@@ -21,30 +17,3 @@ class Solution {
         arr[0]=last;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution ob;
-        ob.rotate(arr);
-        for (long long i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
